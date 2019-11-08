3360d85ee97d632a4e1fda044ba85279634b8663
Louis DeJardin <lodejard@microsoft.com>
2019-11-08 11:39:34 -0800

    Removing commented material

--- terraform/aks/modules/cluster/main.tf
+++ terraform/aks/modules/cluster/main.tf
@@ -110,32 +110,3 @@ resource "azurerm_kubernetes_cluster" "main" {
         max_count       = 100
     }
 }
-
-# resource "kubernetes_namespace" "falcon_system" {
-#     metadata {
-#         name = "falcon-system"
-#     }
-# }
-
-# resource "kubernetes_service_account" "tiller" {
-#     metadata {
-#         name = "tiller"
-#         namespace = "falcon-system"
-#     }
-# }
-
-# resource "kubernetes_cluster_role_binding" "tiller" {
-#   metadata {
-#     name = "falcon-system-tiller"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "tiller"
-#     namespace = "falcon-system"
-#   }
-# }
